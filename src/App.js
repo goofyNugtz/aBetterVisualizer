3c1020fe2ac1f7c036dca013480fb0f5fac7effe
goofyNugtz <rahulranjan25.RR@gmail.com>
2021-03-13 21:24:15 +0530

    viz layout complete

--- src/App.js
+++ src/App.js
@@ -20,8 +20,6 @@ class App extends Component {
     /* console.log(array); */
   }
 
-  
-  
   componentDidMount(){
     this.resetArray();
   }
--- src/Visualizer/Visualizer.js
+++ src/Visualizer/Visualizer.js
@@ -1,13 +1,13 @@
 import React from 'react';
+import './Visualizer.css';
 
 const visualizer = (props) => {
-    
 
     return (
         <div>
             {props.values.map((value, index) => {
                 return(
-                <div className = "bars" key = {index}>
+                <div className = "bars" key = {index} style = {{height: `${value}px`}}>
                     {value}
                 </div>
             )})}
